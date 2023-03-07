7543c6dce90c58aff0315d266579d7cd30fed907
“adrianmaryniewski” <adrian@maryniewski.pl>
2023-03-07 16:16:27 +0100

    aktualizacja repo

---
+++ cypress/e2e/TS1.cy.js
@@ -0,0 +1,15 @@
+describe('test description', () => {
+
+    // create beforeEach method
+    beforeEach(() => {
+        cy.clearLocalStorage()
+    })
+
+    it('should be able to setup filters', () => {
+
+    })
+})
+
+// check github account in the terminal
+
+// You can check your GitHub account in the terminal by using the git command. For example, you can use the command "git config --list" to view your GitHub account information.
